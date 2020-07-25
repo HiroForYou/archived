560277a1e2ff2718cef618bd2928f678a2e85d5c
prabhuomkar <prabhuomkar@pm.me>
2020-07-25 21:46:27 +0530

    Added routes and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to TorchExpo!</h1>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about" exact>
+          <About />
+        </Route>
+        <Error />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
