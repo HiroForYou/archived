f3d71805bc88162978a0ed8ada1df5578d009cea
prabhuomkar <prabhuomkar@pm.me>
2020-08-08 16:28:43 +0530

    Travis CI init

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders landing page', () => {
   const { getAllByText } = render(<App />);
   const elements = getAllByText(/TorchExpo/i);
 });
